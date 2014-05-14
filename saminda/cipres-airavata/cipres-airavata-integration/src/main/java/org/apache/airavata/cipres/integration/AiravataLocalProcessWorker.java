7b7737179f156b5f309d9e9ef9fd36fba2bbb17c
Saminda Wijeratne <samindaw@gmail.com>
2014-05-14 08:56:59 -0700

    changes to add new API code

--- saminda/cipres-airavata/cipres-airavata-integration/src/main/java/org/apache/airavata/cipres/integration/AiravataLocalProcessWorker.java
+++ saminda/cipres-airavata/cipres-airavata-integration/src/main/java/org/apache/airavata/cipres/integration/AiravataLocalProcessWorker.java
@@ -2,18 +2,23 @@ package org.apache.airavata.cipres.integration;
 
 import java.io.File;
 import java.io.InputStream;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-import org.apache.airavata.client.AiravataAPIFactory;
+import org.apache.airavata.api.Airavata;
+import org.apache.airavata.api.Airavata.Client;
+import org.apache.airavata.api.client.AiravataClientFactory;
 import org.apache.airavata.client.api.AiravataAPI;
-import org.apache.airavata.client.api.ExperimentAdvanceOptions;
-import org.apache.airavata.client.api.exception.AiravataAPIInvocationException;
-import org.apache.airavata.client.impl.PasswordCallBackImpl;
-import org.apache.airavata.registry.api.workflow.WorkflowExecutionStatus.State;
-import org.apache.airavata.workflow.model.wf.WorkflowInput;
+import org.apache.airavata.common.exception.ApplicationSettingsException;
+import org.apache.airavata.model.util.ExperimentModelUtil;
+import org.apache.airavata.model.workspace.experiment.AdvancedOutputDataHandling;
+import org.apache.airavata.model.workspace.experiment.ComputationalResourceScheduling;
+import org.apache.airavata.model.workspace.experiment.DataObjectType;
+import org.apache.airavata.model.workspace.experiment.Experiment;
+import org.apache.airavata.model.workspace.experiment.JobState;
+import org.apache.airavata.model.workspace.experiment.JobStatus;
+import org.apache.airavata.model.workspace.experiment.UserConfigurationData;
 import org.ngbw.sdk.api.tool.FileHandler;
 import org.ngbw.sdk.database.RunningTask;
 import org.ngbw.sdk.tool.BaseProcessWorker;
@@ -21,18 +26,12 @@ import org.ngbw.sdk.tool.BaseProcessWorker;
 public class AiravataLocalProcessWorker extends BaseProcessWorker {
 	private AiravataAPI api;
 	private FileHandler fileHandler;
-	private AiravataAPI getAPI(){
-		if (api==null){
-			try {
-				AiravataAPIFactory.getAPI(new URI(getParameter("serverURL")),getParameter("gateway"), getParameter("username"),new PasswordCallBackImpl(getParameter("username"), getParameter("password")));
-			} catch (AiravataAPIInvocationException e) {
-				e.printStackTrace();
-			} catch (URISyntaxException e) {
-				e.printStackTrace();
-			}
-		}
-		return api;
-	}
+	protected Airavata.Client getClient() throws ApplicationSettingsException {
+        String THRIFT_SERVER_HOST = getParameter("thrift.server.host");
+        int THRIFT_SERVER_PORT = Integer.parseInt(getParameter("thrift.server.port"));
+        return AiravataClientFactory.createAiravataClient(THRIFT_SERVER_HOST, THRIFT_SERVER_PORT);
+
+    }
 	
 	private String getParameter(String key){
 		return m_tool.getToolResource().getParameters().get(key);
@@ -48,25 +47,38 @@ public class AiravataLocalProcessWorker extends BaseProcessWorker {
 	
 	protected String submitImpl() throws Exception {
 		RunningTask runningTask = getRunningTask();
-		AiravataAPI api = getAPI();
+		Client client = getClient();
 		File workingDir = new File(new File(getParameter("baseWorkingDir")),m_jobHandle);
 		String staticDirKey = m_jobHandle+"/staticDir";
-		api.getAiravataManager().addConfiguration(staticDirKey, workingDir.toString(), null);
-		String workflowName=runningTask.getToolId();
-		List<WorkflowInput> workflowInputs = api.getWorkflowManager().getWorkflowInputs(workflowName);
-		for (WorkflowInput workflowInput : workflowInputs) {
-			if (runningTask.parameters().containsKey(workflowInput.getName())){
-				if (runningTask.parameters().containsKey(workflowInput.getName())) {
-					workflowInput.setValue(runningTask.parameters().get(
-							workflowInput.getName()));
-				}
-			}
+		Map<String, String> parameters = runningTask.parameters();
+        List<DataObjectType> exInputs = new ArrayList<DataObjectType>();
+		for (String parameterName : parameters.keySet()) {
+			DataObjectType input = new DataObjectType();
+	        input.setKey(parameterName);
+	        input.setValue(parameters.get(parameterName));
+	        exInputs.add(input);
 		}
+		Experiment experiment = ExperimentModelUtil.createSimpleExperiment("default", "admin", runningTask.getTask().getLabel(), runningTask.getTask().getLabel(), runningTask.getToolId(), exInputs);
 		//TODO setup tool inputFile location to the correct WorkflowInput parameter
-		ExperimentAdvanceOptions options = api.getExecutionManager().createExperimentAdvanceOptions();
-		options.setCustomExperimentId(m_jobHandle);
-		String experimentId = api.getExecutionManager().runExperiment(workflowName, workflowInputs, options);
-		runningTask.setRemoteJobId(experimentId);
+		List<DataObjectType> exOut = new ArrayList<DataObjectType>();
+        DataObjectType output = new DataObjectType();
+        output.setKey("echo_output");
+        output.setValue("");
+        exOut.add(output);
+        experiment.setExperimentOutputs(exOut);
+        ComputationalResourceScheduling scheduling = ExperimentModelUtil.createComputationResourceScheduling("localhost", 1, 1, 1, "normal", 0, 0, 1, "sds128");
+        scheduling.setResourceHostId("localhost");
+        UserConfigurationData userConfigurationData = new UserConfigurationData();
+        userConfigurationData.setAiravataAutoSchedule(false);
+        userConfigurationData.setOverrideManualScheduledParams(false);
+        userConfigurationData.setComputationalResourceScheduling(scheduling);
+        AdvancedOutputDataHandling advanceOutputDataHandling = new AdvancedOutputDataHandling();
+        advanceOutputDataHandling.setOutputDataDir(workingDir.toString());
+		userConfigurationData.setAdvanceOutputDataHandling(advanceOutputDataHandling);
+        experiment.setUserConfigurationData(userConfigurationData);
+        String experimentId = client.createExperiment(experiment);
+        client.launchExperiment(experimentId, "tmpToken");
+        runningTask.setRemoteJobId(experimentId);
 		return experimentId;
 	}
 
@@ -75,12 +87,19 @@ public class AiravataLocalProcessWorker extends BaseProcessWorker {
 	 * ones are done, and returns that list.
 	 */
 	public List<RunningTask> checkJobsOnResource(List<RunningTask> running_tasks) throws Exception {
+		Client client = getClient();
 		List<RunningTask> completedTasks=new ArrayList<RunningTask>();
 		for (RunningTask runningTask : running_tasks) {
-			State state = getAPI().getProvenanceManager().getExperimentData(runningTask.getRemoteJobId()).getState();
-			if (state == State.DONE || state == State.FINISHED || state == State.FAILED){
-				completedTasks.add(runningTask);
-			}
+			Map<String, JobStatus> jobStatuses = client.getJobStatuses(runningTask.getRemoteJobId());
+            for (String key : jobStatuses.keySet()) {
+                JobStatus jobStatus = jobStatuses.get(key);
+                if (jobStatus != null) {
+                    if (JobState.COMPLETE==jobStatus.getJobState() || JobState.FAILED==jobStatus.getJobState()) {
+                    	completedTasks.add(runningTask);
+                    	break;
+                    }
+                }
+            }
 		}
 		return completedTasks;
 	}
